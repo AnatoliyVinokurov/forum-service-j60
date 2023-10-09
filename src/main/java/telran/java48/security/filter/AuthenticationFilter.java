e17df1e2bd5fc33ec07c268346ba3e7eafa822d5
AnatoliyVinokurov <anatolij.isit@gmail.com>
2023-10-09 19:24:50 +0300

    added HttpServlet Request Wrapper

--- src/main/java/telran/java48/security/filter/AuthenticationFilter.java
+++ src/main/java/telran/java48/security/filter/AuthenticationFilter.java
@@ -1,6 +1,7 @@
 package telran.java48.security.filter;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Base64;
 
 import javax.servlet.Filter;
@@ -9,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.mindrot.jbcrypt.BCrypt;
@@ -34,18 +36,48 @@ public class AuthenticationFilter implements Filter {
 		// System.out.println(request.getServletPath()+ " -> " + request.getMethod());
 		// System.out.println(request.getHeader("Authorization"));
 
-		String[] credentials = getCredentials(request.getHeader("Authorization"));
-		UserAccount userAccount = userAccountRepository.findById(credentials[0]).orElseThrow(UserNotFoundExeption::new);
-		if(!BCrypt.checkpw(credentials[1], userAccount.getPassword())) {
-			throw new UserNotFoundExeption();
+		if (checkEndPoint(request.getMethod(), request.getServletPath())) {
+			try {
+				String[] credentials = getCredentials(request.getHeader("Authorization"));
+				UserAccount userAccount = userAccountRepository.findById(credentials[0])
+						.orElseThrow(RuntimeException::new);
+				if (!BCrypt.checkpw(credentials[1], userAccount.getPassword())) {
+					throw new RuntimeException();
+				}
+				request = new WeappedRequest(request, userAccount.getLogin());
+			} catch (Exception e) {
+				response.sendError(401);
+				return;
+			} 
+			
+			
 		}
 		chain.doFilter(request, response);
+	}
+
+	private boolean checkEndPoint(String method, String servletPath) {
+		return !("POST".equalsIgnoreCase(method) && servletPath.equals("/account/register"));
 	}
 
 	private String[] getCredentials(String header) {
 		String token = header.substring(6);
 		String decode = new String(Base64.getDecoder().decode(token));
 		return decode.split(":");
+	}
+	
+	private class WeappedRequest extends HttpServletRequestWrapper{
+		String login;
+
+		public WeappedRequest(HttpServletRequest request, String login) {
+			super(request);
+			this.login = login;
+		}
+		
+		@Override
+		public Principal getUserPrincipal() {
+			return () -> login ;
+		}
+		
 	}
 
 }
